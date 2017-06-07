4bfa0f70f86956cb5ca0c0e656371feee9c69304
Tim Chase <tchase4544@gmail.com>
2017-06-07 18:31:45 -0400

    full CRUD for house and characters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,25 +1,64 @@
 class CharactersController < ApplicationController
+  def all
+    @people = Character.all.order(:house_id)
+  end
+
   def index
     @people = Character.all
+    @house = House.find(params[:house_id])
   end
 
   def create
+    @house = House.find(params[:house_id])
+    @person = @house.characters.new(char_params)
+    if @person.save
+      flash[:notice] = "#{@person.name} added to House #{@house.name}"
+      redirect_to house_path(@house)
+    else
+      flash[:alert] = "Could not create character"
+    end
   end
   
   def new
-  end
-
-  def edit
+    @house = House.find(params[:house_id])
+    @person = Character.new
   end
 
   def show
     @person = Character.find(params[:id])
+    @house = House.find(params[:house_id])
+  end
+
+  def edit
+    @person = Character.find(params[:id])
+    @house = House.find(params[:house_id])
   end
 
   def update
+    @house = House.find(params[:house_id])
+    @person = @house.characters.find(params[:id])
+    if @person.update(char_params)
+      flash[notice] = "#{@person.name} updated"
+      redirect_to 
+    else
+      flash[:alert] = "That person did NOT update"
+    end
   end
 
   def destroy
+    @house = House.find(params[:house_id])
+    @person = @house.characters.find(params[:id])
+    if @person.destroy
+      flash[:notice] = "And now their watch has ended"
+      redirect_to house_characters_path
+    else
+      flash[:alert] = "Hmm...Hard to kill this one is"
+    end
   end
+
+  private
+    def char_params
+      params.require(:character).permit(:name,:title,:img_url)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: 'houses#index'
-get '/characters' => 'characters#index'
+get '/characters' => 'characters#all'
 resources :houses do
 	resources :characters
 end
-
 end
